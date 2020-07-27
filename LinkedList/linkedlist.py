78a0d0cca6e27360fe71e6e38ed01a0e4ca3c787
unknown <joel.reujoe@gmail.com>
2020-07-27 10:48:57 +0530

    added mor functions

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -39,6 +39,84 @@ class LinkedList:
             last = last.next
         last.next = new_node
 
+    def deleteANode(self, key):
+        temp = self.head
+        
+        if(temp is not None):
+            if(temp.data == key):
+                self.head = temp.next
+                temp = None
+                return
+
+        while(temp is not None):
+            if(temp.data == key):
+                break
+            prev = temp
+            temp = temp.next
+
+        if temp == None:
+            return
+
+        prev.next = temp.next
+        temp = None
+
+    def getLengthRec(self, node):
+        if (not node):
+            return 0
+        else:
+            return 1 + self.getLengthRec(node.next)
+
+    def deleteANodeByPosition(self, pos):
+        if self.head == None:
+            return
+
+        temp = self.head
+        if pos == 0:
+            self.head = temp.next
+            temp = None
+            return
+
+        for i in range(pos-1):
+            temp = temp.next
+            if temp is None:
+                break
+        
+        if temp is None:
+            return
+        if temp.next is None:
+            return
+
+        next = temp.next.next
+        temp.next = None
+        temp.next = next
+
+    def deleteList(self):
+        current = self.head
+        while current:
+            prev = current.next
+            del current.data
+            current = prev
+
+    def printLength(self):
+        temp = self.head
+        count = 0
+
+        while(temp):
+            count += 1
+            temp = temp.next
+        
+        print(count)
+
+    def search(self, x):
+        temp = self.head
+        while(temp):
+            if(temp.data==x):
+                return True
+            temp = temp.next
+        
+        return False
+
+
 
 if __name__ == '__main__':
     llist = LinkedList()
@@ -51,3 +129,18 @@ if __name__ == '__main__':
     llist.insertAfter(llist.head, 6)
     llist.addNodeAtEnd(3)
     llist.printList()
+    print("delete")
+    llist.deleteANode(3)
+    llist.deleteANode(5)
+    llist.deleteANode(7)
+    llist.deleteANodeByPosition(3)
+    llist.deleteANodeByPosition(0)
+    llist.printList()
+    llist.insertAfter(llist.head,7)
+    llist.printList()
+    llist.printLength()
+    print(llist.getLengthRec(llist.head))
+    if(llist.search(7)):
+        print("yes")
+    else:
+        print("no")
